ae9c6a2dc4da1712da40c6733d570dca2361d918
Woody 1024800 <1024800@student.sae.edu.au>
2023-11-23 13:45:08 +0800

    Centipede Move Values
    +left & right buttonmap arrays
    +left & right movespeed values
    +Ratio value between left & right (For rotation)

--- Assets/Scripts/WW_CentipedeMovement01.cs
+++ Assets/Scripts/WW_CentipedeMovement01.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class WW_CentipedeMovement01 : MonoBehaviour
@@ -5,149 +7,156 @@ public class WW_CentipedeMovement01 : MonoBehaviour
     //
     //                      THIS SCRIPT STARTED AS AN EDIT OF CAMERON MOORE'S ORIGINAL SCRIPT CREDIT TO THEM
     //                                  THOUGH QUICKLY BECAME MOSTLY A NEW SCRIPT
+
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float leftMoveSpeed;
+    [SerializeField] private float rightMoveSpeed;
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float testRotValue;
 
 
     public KeyCode[] leftMoveButton;
     public KeyCode[] rightMoveButton;
     public Vector2 curVol;
 
-    [SerializeField] private bool[] steps;
+    [SerializeField] private bool[] leftSteps;
+    [SerializeField] private bool[] rightSteps;
 
     private void Start()
     {
-        steps = new bool[4];
+        leftSteps = new bool[4];
+        rightSteps = new bool[4];
     }
 
     private void Update()
     {
+
+        float tempRatio = Mathf.InverseLerp(0, leftMoveSpeed + rightMoveSpeed, leftMoveSpeed);
+        testRotValue = Mathf.Lerp(-100f, 100f, tempRatio);
         curVol = GetComponent<Rigidbody>().velocity;
 
-        if (Input.GetKey(leftMoveButton[0]))
+        if (Input.GetKeyDown(leftMoveButton[0]))
         {
             print(1);
-            if (!steps[0])
+            if (!leftSteps[0])
             {
-                MoveCentipede();
+                leftMoveSpeed++;
             }
-            else
+            else if (leftMoveButton.Any(key => Input.GetKey(key)))
             {
-                SlowCentipede();
+                leftMoveSpeed--;
             }
-            ResetSteps();
-            steps[0] = true;
+            ResetSteps(leftSteps);
+            leftSteps[0] = true;
         }
 
-        if (Input.GetKey(leftMoveButton[1]))
+        if (Input.GetKeyDown(leftMoveButton[1]))
         {
             print(2);
-            if (steps[0])
+            if (leftSteps[0])
             {
-                MoveCentipede();
+                leftMoveSpeed++;
+
             }
-            else
+            else if (leftMoveButton.Any(key => Input.GetKey(key)))
             {
-                SlowCentipede();
+                leftMoveSpeed--;
             }
-            ResetSteps();
-            steps[1] = true;
+            ResetSteps(leftSteps);
+            leftSteps[1] = true;
         }
 
-        if (Input.GetKey(leftMoveButton[2]))
+        if (Input.GetKeyDown(leftMoveButton[2]))
         {
             print(3);
-            if (steps[0] || steps[1])
+            if (leftSteps[0] || leftSteps[1])
             {
-                MoveCentipede();
+                leftMoveSpeed++;
             }
-            else
+            else if (leftMoveButton.Any(key => Input.GetKey(key)))
             {
-                SlowCentipede();
+                leftMoveSpeed--;
             }
-            ResetSteps();
-            steps[2] = true;
+            ResetSteps(leftSteps);
+            leftSteps[2] = true;
         }
 
-        if (Input.GetKey(leftMoveButton[3]))
+        if (Input.GetKeyDown(leftMoveButton[3]))
         {
             print(4);
-            if (steps[1] || steps[2])
+            if (leftSteps[1] || leftSteps[2])
             {
-                MoveCentipede();
+                leftMoveSpeed++;
             }
-            else
+            else if (leftMoveButton.Any(key => Input.GetKey(key)))
             {
-                SlowCentipede();
+                leftMoveSpeed--;
             }
-            ResetSteps();
-            steps[3] = true;
+            ResetSteps(leftSteps);
+            leftSteps[3] = true;
         }
 
-        if (Input.GetKey(rightMoveButton[0]))
+        if (Input.GetKeyDown(rightMoveButton[0]))
         {
             print(1);
-            if (!steps[0])
+            if (!rightSteps[0])
             {
-                MoveCentipede();
+                rightMoveSpeed++;
             }
-            else
+            else if (rightMoveButton.Any(key => Input.GetKey(key)))
             {
-                SlowCentipede();
+                rightMoveSpeed--;
             }
-            ResetSteps();
-            steps[0] = true;
+            ResetSteps(rightSteps);
+            rightSteps[0] = true;
         }
 
-        if (Input.GetKey(rightMoveButton[1]))
+        if (Input.GetKeyDown(rightMoveButton[1]))
         {
             print(2);
-            if (steps[0])
+            if (rightSteps[0])
             {
-                MoveCentipede();
+                rightMoveSpeed++;
+
             }
-            else
+            else if (rightMoveButton.Any(key => Input.GetKey(key)))
             {
-                SlowCentipede();
+                rightMoveSpeed--;
             }
-            ResetSteps();
-            steps[1] = true;
+            ResetSteps(rightSteps);
+            rightSteps[1] = true;
         }
 
-        if (Input.GetKey(rightMoveButton[2]))
+        if (Input.GetKeyDown(rightMoveButton[2]))
         {
             print(3);
-            if (steps[0] || steps[1])
+            if (rightSteps[0] || rightSteps[1])
             {
-                MoveCentipede();
+                rightMoveSpeed++;
+
             }
-            else
+            else if (rightMoveButton.Any(key => Input.GetKey(key)))
             {
-                SlowCentipede();
+                rightMoveSpeed--;
             }
-            ResetSteps();
-            steps[2] = true;
+            ResetSteps(rightSteps);
+            rightSteps[2] = true;
         }
 
-        if (Input.GetKey(rightMoveButton[3]))
+        if (Input.GetKeyDown(rightMoveButton[3]))
         {
             print(4);
-            if (steps[1] || steps[2])
+            if (rightSteps[1] || rightSteps[2])
             {
-                MoveCentipede();
+                rightMoveSpeed++;
             }
-            else
+            else if (rightMoveButton.Any(key => Input.GetKey(key)))
             {
-                SlowCentipede();
+                rightMoveSpeed--;
             }
-            ResetSteps();
-            steps[3] = true;
+            ResetSteps(rightSteps);
+            rightSteps[3] = true;
         }
-
-        /*   else if (!Input.anyKey)
-           {
-               ResetSteps();
-           }*/
     }
 
     private void MoveCentipede()
@@ -161,7 +170,7 @@ public class WW_CentipedeMovement01 : MonoBehaviour
         GetComponent<Rigidbody>().velocity -= Vector3.forward * moveSpeed;
     }
 
-    private void ResetSteps()
+    private void ResetSteps(bool[] steps)
     {
         for (int i = 0; i < steps.Length; i++)
         {
